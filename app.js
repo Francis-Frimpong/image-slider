16745beb79b281cab0d11d754bab7ab7f880ff86
Francis <brakay1136@gmail.com>
2025-05-12 11:34:59 -0700

    (90% of app functionality done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const imageBox = document.querySelector('.image-box')
+const previousBtn = document.querySelector('.fa-chevron-left')
 const nextBtn = document.querySelector('.fa-chevron-right')
+const indicators = document.querySelectorAll('.indicator')
 
 const imageList = [
     "images/ninja-1.png",
@@ -16,17 +18,54 @@ imageBox.appendChild(createImage);
 
 function nextImage() {
     countImg++
-
+    
     if(countImg >= imageList.length){
         countImg = 0;
     }
-
+    
     createImage.src = imageList[countImg];
     createImage.innerHTML = ""
     imageBox.appendChild(createImage);
+    linkIndicatorToImage()
+
 
 }
+function previousImage() {
+    countImg--
+    
+    if(countImg < 0){
+        countImg = 0;
+    }
+    
+    createImage.src = imageList[countImg];
+    createImage.innerHTML = ""
+    imageBox.appendChild(createImage);
+    linkIndicatorToImage()
+}
+
+function linkIndicatorToImage() {
+    indicators.forEach((indicator, index) => {
+        imageList[index]
+        if (index === countImg) {
+            indicator.classList.add('indicate-color')
+        } else {
+            indicator.classList.remove('indicate-color')
+        }
+
+    });
+}
+linkIndicatorToImage()
 
 
+
+setInterval(() => {
+    nextImage()
+    linkIndicatorToImage()
+
+    
+},4000)
+
+
+previousBtn.addEventListener('click', previousImage);
 nextBtn.addEventListener('click', nextImage);
 
