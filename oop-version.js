392228fac4f3432fb7f29fa4f4a0df52cf01713f
Francis <brakay1136@gmail.com>
2025-09-10 10:26:20 -0700

    oop version of image slider done

--- oop-version.js
+++ oop-version.js
@@ -22,11 +22,45 @@ class ImageSlider {
     this.createImage.src = this.imageList[this.countImg];
     this.createImage.innerHTML = "";
     imageBox.appendChild(this.createImage);
+    this.linkIndicatorToImage();
+  }
+
+  previousImage() {
+    this.countImg--;
+
+    if (this.countImg < 0) {
+      this.countImg = 0;
+    }
+
+    this.createImage.src = this.imageList[this.countImg];
+    this.createImage.innerHTML = "";
+    imageBox.appendChild(this.createImage);
+    this.linkIndicatorToImage();
+  }
+
+  linkIndicatorToImage() {
+    indicators.forEach((indicator, index) => {
+      this.imageList[index];
+      if (index === this.countImg) {
+        indicator.classList.add("indicate-color");
+      } else {
+        indicator.classList.remove("indicate-color");
+      }
+    });
+  }
+
+  automaticSlider() {
+    setInterval(() => {
+      this.nextImage();
+      this.linkIndicatorToImage();
+    }, 7000);
   }
 }
 
 const imageBox = document.querySelector(".image-box");
 const nextBtn = document.querySelector(".fa-chevron-right");
+const previousBtn = document.querySelector(".fa-chevron-left");
+const indicators = document.querySelectorAll(".indicator");
 
 const imageList = [
   "images/ninja-1.png",
@@ -35,5 +69,8 @@ const imageList = [
 ];
 
 const imageSlider = new ImageSlider(imageList);
+imageSlider.linkIndicatorToImage();
+imageSlider.automaticSlider();
 
+previousBtn.addEventListener("click", () => imageSlider.previousImage());
 nextBtn.addEventListener("click", () => imageSlider.nextImage());
